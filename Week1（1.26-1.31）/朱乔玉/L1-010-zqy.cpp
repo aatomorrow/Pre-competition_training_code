3f7157ef6d2914ef3da8137b5e18f2cd56e79416
Zhu Qiaoyu <165273096+QiaoyuZhuVCC@users.noreply.github.com>
2026-01-27 23:18:49 +0800

    update

---
+++ Week1（1.26-1.31）/朱乔玉/L1-010-zqy.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> nums(3);
+    for(int i = 0; i < 3; i++){
+        cin >> nums[i];
+    }
+    sort(nums.begin(),nums.end());
+    cout << nums[0] << "->" << nums[1] << "->" << nums[2];
+    return 0;
+}
